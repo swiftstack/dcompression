c468c1989cdbd8ed0e59d9dab1a2ab669ecf8e83
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-14 18:08:50 +0000

    [Cleanup] Delete copyright notice

--- Sources/Compression/CRC32.swift
+++ Sources/Compression/CRC32.swift
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 public struct CRC32 {
     static let table: [UInt32] = {
         var table = [UInt32](repeating: 0, count: 256)
--- Sources/Compression/Constants.swift
+++ Sources/Compression/Constants.swift
@@ -1,14 +1,3 @@
-/******************************************************************************
- *                                                                            *
- * Tris Foundation disclaims copyright to this source code.                   *
- * In place of a legal notice, here is a blessing:                            *
- *                                                                            *
- *     May you do good and not evil.                                          *
- *     May you find forgiveness for yourself and forgive others.              *
- *     May you share freely, never taking more than you give.                 *
- *                                                                            *
- ******************************************************************************/
-
 struct LengthBitLengths {
     static let order = [ // Order of the bit length code lengths
         16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15

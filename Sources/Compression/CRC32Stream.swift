9484ac22f4130d4861150f0363bf6bbf78b561c1
Tony Freeman <tonyfreeman@users.noreply.github.com>
2019-01-16 13:34:40 +0000

    [Deflate] Update CRC32Stream

--- Sources/Compression/CRC32Stream.swift
+++ Sources/Compression/CRC32Stream.swift
@@ -1,38 +1,41 @@
 import Stream
 
-public class CRC32Stream {
-    private var c: UInt32 = 0xffffffff
+public class CRC32Stream: OutputStream {
+    var c: UInt32 = 0xffff_ffff
 
     public var value: UInt32 {
-        return c ^ 0xffffffff
+        return c ^ 0xffff_ffff
     }
 
+    @inline(__always)
     public func write(_ byte: UInt8) throws {
         c = CRC32.table[Int((c ^ UInt32(byte)) & 0xff)] ^ (c >> 8)
     }
 
+    @inline(__always)
+    public func write(_ bytes: UnsafeRawBufferPointer) throws {
+        try bytes.forEach(write)
+    }
+
+    @inlinable
+    public func write(
+        from pointer: UnsafeRawPointer,
+        byteCount count: Int) throws -> Int
+    {
+        try write(.init(start: pointer, count: count))
+        return count
+    }
+}
+
+extension CRC32Stream {
+    @inlinable
     public func write<T: BinaryInteger>(_ value: T) throws {
         var value = value
         try withUnsafeBytes(of: &value, write)
     }
 
-    func write(_ bytes: [UInt8]) throws {
+    @inlinable
+    public func write(_ bytes: [UInt8]) throws {
         try bytes.withUnsafeBytes(write)
-    }
-
-    func write(_ bytes: UnsafeRawBufferPointer) throws {
-        for byte in bytes {
-            c = CRC32.table[Int((c ^ UInt32(byte)) & 0xff)] ^ (c >> 8)
-        }
-    }
-}
-
-extension CRC32Stream: OutputStream {
-    public func write(
-        from buffer: UnsafeRawPointer,
-        byteCount: Int) throws -> Int
-    {
-        try write(UnsafeRawBufferPointer(start: buffer, count: byteCount))
-        return byteCount
     }
 }

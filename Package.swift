5571a794eec6d8fd1b5de0ecaf6671e61dc0f759
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 05:46:04 +0300

    [Package] Remove unsafe flags

--- Package.swift
+++ Package.swift
@@ -16,11 +16,7 @@ let package = Package(
         .target(
             name: "DCompression",
             dependencies: ["Stream"],
-            path: "./Sources/Compression",
-            swiftSettings: [
-                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
-                .unsafeFlags(["-Xfrontend", "-enable-experimental-concurrency"])
-            ]),
+            path: "./Sources/Compression"),
     ]
 )
 
@@ -44,11 +40,7 @@ func addTest(target: String, name: String) {
         .executableTarget(
             name: "Tests/\(target)/\(name)",
             dependencies: ["DCompression", "Test"],
-            path: "Tests/\(target)/\(name)",
-            swiftSettings: [
-                .unsafeFlags(["-Xfrontend", "-disable-availability-checking"]),
-                .unsafeFlags(["-Xfrontend", "-enable-experimental-concurrency"])
-            ]))
+            path: "Tests/\(target)/\(name)"))
 }
 
 // MARK: - custom package source
